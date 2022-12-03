d11ffc18462cde8da04ec3ba1e4499fbdcb52bc6
Mohammad Kheirkhahan <m.kh.mkh.00513@gmail.com>
2022-12-03 18:02:51 +0330

    psr-4-autoloader-is-ok

---
+++ App/Core/Requset.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Core;
+
+class Requset{
+    public function __construct()
+    {
+        echo "RequestClass";
+    }
+}
--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+include "vendor/autoload.php";
+
 #front controller
 // echo "Hello World !!!";
-echo $_SERVER['REQUEST_URI'];
+// echo $_SERVER['REQUEST_URI'];
+
+new App\Core\Requset(); 
